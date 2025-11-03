50a06b2ae003effa3a6da6f74a022157008ec0c5
KwangYongPark <mail.binz@gmail.com>
2025-11-03 22:00:10 +0900

    공통수정파일 업로드

---
+++ src/main/java/dev/binz/reference/utils/GitSameFileFix.java
@@ -0,0 +1,14 @@
+package dev.binz.reference.utils;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+public class GitSameFileFix {
+    String Data;
+
+    public String prinf(){
+        return "test";
+    }
+}
